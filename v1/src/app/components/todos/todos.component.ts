019ebbbd5a56e181792be81749f8d32fe9d8827e
taeksoo-shin <cu7898@gmail.com>
2024-04-11 22:05:44 +0900

    Feat add todo

--- v1/src/app/components/todos/todos.component.ts
+++ v1/src/app/components/todos/todos.component.ts
@@ -8,8 +8,29 @@ import { Todo } from '../../models/todo';
 })
 export class TodosComponent {
   todos: Todo[] = [
-    new Todo('hello 1'),
-    new Todo('hello 2'),
-    new Todo('hello 3'),
+    new Todo('default todo1'),
+    new Todo('default todo2'),
+    new Todo('default todo3'),
   ]
+
+  addTodoByKeyupEnter = (event: Event): void => {
+    const input = event.target as HTMLInputElement;
+    const title: string = input.value;
+
+    if (!title) {
+      return;
+    }
+
+    this.addTodo(title);
+    this.clearInputValue(input);
+  }
+
+  clearInputValue = (input: HTMLInputElement): void => {
+    input.value = '';
+  }
+
+  addTodo = (title: string): void => {
+    const todo = new Todo(title);
+    this.todos.push(todo);
+  }
 }
