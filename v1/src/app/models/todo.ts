f00a0d7285faa806cc72784cf53efd7d3f7be6e7
taeksoo-shin <cu7898@gmail.com>
2024-04-11 21:51:32 +0900

    Feat check todo

--- v1/src/app/models/todo.ts
+++ v1/src/app/models/todo.ts
@@ -1,7 +1,13 @@
 export class Todo {
   title: string;
+  done: boolean;
 
-  constructor(title: string) {
+  constructor(title: string, done: boolean = false) {
     this.title = title;
+    this.done = done;
+  }
+
+  toggleDone = (): void => {
+    this.done = !this.done;
   }
 }
